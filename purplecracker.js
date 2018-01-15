c4cf107c71465f0cfd273e9999ba0e5d63e7de7d
ayuningnareswari <31541122+ayuningnareswari@users.noreply.github.com>
2018-01-15 14:53:37 +0700

    Create purplecracker.js

---
+++ purplecracker.js
@@ -0,0 +1,1 @@
+document.write('<script type="text/javascript" src="http://bpmaker.giffy.me/userdata/cmn/parts53.js?us=48251&id=809&bic=4&nc=0&im1=b1-1409276043.png&im2=b2-1409276152.png&im3=b3-1409276269.png&im4=b4-1409276365.png&c1=30&c2=2&c3=4&c5=1&c7=5&c8=0"></script>');document.close();
