998e4308269c11731905f624c028d259bdf69d8d
ayuningnareswari <31541122+ayuningnareswari@users.noreply.github.com>
2018-01-15 18:30:41 +0700

    Create angelstar.js

---
+++ angelstar.js
@@ -0,0 +1,1 @@
+document.write('<script type="text/javascript" src="http://bpmaker.giffy.me/userdata/cmn/parts83.js?us=25308&id=312&bic=6&nc=0&im1=b1-1397071738.gif&im2=b2-1397071748.gif&im3=b3-1397072248.png&im4=b4-1397072248.png&im5=b5-1397072248.png&im6=b6-1397072248.png&c2=2&c8=0&c14=1&c23=3"></script>');document.close();
