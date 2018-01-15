ad3624f1ee99c9bbe73a1b84ca1aa06fb496cc66
ayuningnareswari <31541122+ayuningnareswari@users.noreply.github.com>
2018-01-15 18:27:40 +0700

    Create soapplonk.js

---
+++ soapplonk.js
@@ -0,0 +1,1 @@
+document.write('<script type="text/javascript" src="http://bpmaker.giffy.me/userdata/cmn/parts57.js?us=15364&id=15&bic=3&nc=0&im1=b1-1350990209.png&im2=b2-1350990209.png&im3=b3-1350990255.png&c1=30&c2=3&c3=0&c8=0"></script>');document.close();
