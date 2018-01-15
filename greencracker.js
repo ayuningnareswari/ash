300a8aff163a149bb80593fc7a27cb3807d75184
ayuningnareswari <31541122+ayuningnareswari@users.noreply.github.com>
2018-01-15 14:56:32 +0700

    Create greencracker.js

---
+++ greencracker.js
@@ -0,0 +1,1 @@
+document.write('<script type="text/javascript" src="http://bpmaker.giffy.me/userdata/cmn/parts53.js?us=48251&id=810&bic=3&nc=0&im1=b1-1409277218.png&im2=b2-1409277348.png&im3=b3-1409277461.png&c1=30&c2=2&c3=4&c5=1&c7=5&c8=0"></script>');document.close();
