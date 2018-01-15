44a5b0077f85eccf2b169f65ce8e8a0ebf6a9def
ayuningnareswari <31541122+ayuningnareswari@users.noreply.github.com>
2018-01-15 15:12:35 +0700

    Create fw1.js

---
+++ fw1.js
@@ -0,0 +1,1 @@
+document.write('<script type="text/javascript" src="http://bpmaker.giffy.me/userdata/cmn/parts173.js?us=46075&id=27&bic=1&nc=0&im1=b1-1390404835.jpg&c18=8&c25=5"></script>');document.close();
