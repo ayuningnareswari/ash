7ca82627e12dcec9401012a25b7f8690903c4f10
ayuningnareswari <31541122+ayuningnareswari@users.noreply.github.com>
2018-01-15 18:52:24 +0700

    Create strawparty.js

---
+++ strawparty.js
@@ -0,0 +1,1 @@
+document.write('<script type="text/javascript" src="http://bpmaker.giffy.me/userdata/cmn/parts69.js?us=47260&id=55&bic=5&nc=0&im1=b1-1395913227.png&im2=b2-1395913244.png&im3=b3-1395913263.png&im4=b4-1395913287.png&im5=b5-1395913346.gif&c2=3&c8=1&c9=15&c10=4&c11=3&c14=10"></script>');document.close();
