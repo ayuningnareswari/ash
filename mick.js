11dbfcceb2ff841401b61940ba7679d02de71033
ayuningnareswari <31541122+ayuningnareswari@users.noreply.github.com>
2018-01-15 19:38:31 +0700

    Create mick.js

---
+++ mick.js
@@ -0,0 +1,1 @@
+document.write('<script type="text/javascript" src="http://bpmaker.giffy.me/userdata/cmn/parts17.js?us=51142&id=1&bic=1&nc=0&im1=b1-1408541100.gif&c1=10&c2=3&c3=0&c8=0&c10=2&c11=3"></script>');document.close();
