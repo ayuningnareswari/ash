a050ef52c541d9c2ec1f97f145a54e80b0278af9
ayuningnareswari <31541122+ayuningnareswari@users.noreply.github.com>
2018-01-15 17:50:53 +0700

    Create butgli.js

---
+++ butgli.js
@@ -0,0 +1,1 @@
+document.write('<script type="text/javascript" src="http://bpmaker.giffy.me/userdata/cmn/parts83.js?us=45294&id=380&bic=4&nc=0&im1=b2-1394181011.png&im2=b3-1394181023.png&im3=b1-1394180969.png&im4=b4-1394181139.png&c2=1&c8=0&c14=1&c23=4"></script>');document.close();
