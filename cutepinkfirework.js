d1f47e78039e242d63893b1fa6e92e0e484a1d2f
ayuningnareswari <31541122+ayuningnareswari@users.noreply.github.com>
2018-01-16 07:32:44 +0700

    Create cutepinkfirework.js

---
+++ cutepinkfirework.js
@@ -0,0 +1,1 @@
+document.write('<script type="text/javascript" src="http://bpmaker.giffy.me/userdata/cmn/parts64.js?us=40596&id=85&bic=3&nc=0&im1=b1-1388175706.gif&im2=b2-1388175732.gif&im3=b3-1388175752.gif&c2=3&c8=0&c9=10&c10=4&c11=3&c14=5"></script>');document.close();
